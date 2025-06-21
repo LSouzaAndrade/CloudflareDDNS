57dd5d8eb03e6c3529492199ce49645605895e8f
Lucas Souza <lucas.souza.andrade66@gmail.com>
2025-06-21 01:25:57 -0300

    Update update_ip.py

--- scripts/update_ip.py
+++ scripts/update_ip.py
@@ -15,5 +15,3 @@ if __name__ == "__main__":
         logger.info(f"IP mudou de {last_ip} para {new_ip}")
         if update_dns(new_ip):
             save_ip(new_ip)
-    else:
-        logger.info("IP não mudou ou falhou ao obter IP atual. Nenhuma ação necessária.")
